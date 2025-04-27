c160430cef5620c81b153fee2db776685ec2ab00
Flavio F <flavio.win@gmail.com>
2025-04-27 16:58:14 -0300

    Chaged Logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,9 +4,13 @@ import { BookOpen } from 'lucide-react';
 const Logo = () => {
   return (
     <div className="flex items-center">
-      <div className="relative h-10 w-10 bg-blue-400 rounded-full flex items-center justify-center overflow-hidden shadow-lg">
-        <div className="absolute inset-0 bg-gradient-to-br from-blue-400 to-blue-500"></div>
-        <BookOpen size={24} className="text-white relative z-10" />
+      <div className="relative h-14 w-14 rounded-full flex items-center justify-center overflow-hidden shadow-lg">
+        <div className="absolute inset-0 bg-gradient-to-br"></div>
+        <img 
+          src="/instituto.jpeg" 
+          alt="Logo" 
+          className="relative z-12 h-12 w-12"
+        />
       </div>
       <div className="ml-2">
         <div className="text-xl font-bold text-blue-500">Instituto Ser Feliz</div>
